068f57a70b34abfd63868c1b79e54159c117bb3c
MyCoder525 <1830964443@qq.com>
2022-06-26 17:33:24 +0800

    初始化阶段也完成

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  lintOnSave: false,
+  devServer: {
+    open: true,
+    port: 1500,
+    host: 'localhost'
+  },
 })
